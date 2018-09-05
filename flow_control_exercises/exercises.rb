7ebf48edb4ab6876a84d8f9bcaff08feba888432
testname <testemail>
2018-09-05 19:16:37 +1000

    done

--- flow_control_exercises/exercises.rb
+++ flow_control_exercises/exercises.rb
@@ -1,26 +1,59 @@
-def evaluate(num)
+# 1.
+# 1. false
+# 2. false
+# 3. false
+# 4. true
+# 5. true
 
+# 2.
 
-  # if num > 0 && num <= 50
-  #   puts 'it is between 0 to 50'
-  # elsif num >= 51 && num <= 100
-  #   puts 'it is between 51 and 100'
-  # else
-  #   puts ' it is above 100'
-  # end
+# def cap(word)
+#   if word.length > 10
+#     word.upcase
+#   else
+#     word
+#   end
+# end
 
-  case num
-  when 0..50
-    puts 'it is between 0 to 50'
-  when 51..100
-    puts 'it is between 51 and 100'
-  else
-    puts 'more than 100'
-  end
+# puts cap('djokovic')
 
-end
+# 3.
+# puts 'enter a number between 0 to 100'
+# num = gets.chomp.to_i
 
-puts 'enter between 0 to 100'
-num = gets.chomp.to_i
+# if num < 0
+#   puts 'thats a negative number...'
+# elsif num <= 50
+#   puts 'the num is between 0 - 50'
+# elsif num <= 100
+#   puts ' the num is between 51 - 100'
+# else
+#   puts ' it is more than 100'
+# end
 
-evaluate(num)
+# 4.
+# 1.FALSE
+# 2.did you get it right
+# 3.Alright now!
+
+# 5.
+# def calculate
+#   puts ' enter num between 0 - 100'
+#   num = gets.chomp.to_i
+#   case num
+#     when 0..50
+#       puts 'thats between 0 and 50'
+#     when 51..100
+#       puts 'thats between 51 and 100'
+#     else
+#       if num < 0
+#         puts 'negative'
+#       elsif num > 100
+#         puts 'more than 100'
+#       end
+#     end
+# end
+
+# calculate
+
+# 6. missing an end
--- methods_exercises/exercise_methods.rb
+++ methods_exercises/exercise_methods.rb
@@ -1,7 +1,33 @@
-def scream(words)
-  words = words + "!!!!"
-  return
-  puts words
-end
+# 1.
+# def greeting(name)
+#   "i am returning a message to #{name}"
+# end
 
-puts scream("Yippeee")
+# puts greeting("tom")
+
+# 2.
+# 1. '2'
+# 2. nil
+# 3. 'joe'
+# 4. 'four'
+# 5. 'nil'
+
+# 3.
+# def multiply(a, b)
+#   a * b
+# end
+
+# 4. nothing because it says to return nothing
+
+# 5.
+# def scream(words)
+#   words = words + "!!!!"
+#   puts words
+# end
+
+# scream("Yippeee")
+
+# still returns nil because puts returns nothing.
+
+# 6.
+# given 1 argument, but the method expects 2
