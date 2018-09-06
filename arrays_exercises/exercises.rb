2e6ec56783d2959bba043eed68ef0874edcf8f4c
testname <testemail>
2018-09-06 10:29:38 +1000

    from arrays_exercises

---
+++ arrays_exercises/exercises.rb
@@ -0,0 +1,56 @@
+# 1.
+# arr = [1, 3, 5, 7, 9, 11]
+# number = 3
+
+# if arr.include?(number)
+#   puts "yes, #{number} is included in #{arr}"
+# end
+
+
+
+# 2.
+# arr = ["b", "a"]
+#    arr = arr.product(Array(1..3))
+#    arr.first.delete(arr.first.last)
+# puts arr
+
+
+# arr = ["b", "a"]
+#    arr = arr.product([Array(1..3)])
+#    arr.first.delete(arr.first.last)
+#    p arr
+
+
+
+# 3.
+# arr = [["test", "hello", "world"],["example", "mem"]]
+# p arr.last.first
+
+
+
+# 4.
+# arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+# 1. 3
+# 2.error
+# 3.8
+
+
+
+# 5.
+# string = "Welcome to America!"
+# a = string[6]
+# b = string[11]
+# c = string[19]
+
+# p a,b,c
+
+# 6.
+# names = ['bob', 'joe', 'susan', 'margaret']
+# names[3] = 'jody'
+
+
+# 7.
+# array = [1,2,3,4,5]
+# new_array = array.map { |x| x + 2 }
+# p array, new_array
