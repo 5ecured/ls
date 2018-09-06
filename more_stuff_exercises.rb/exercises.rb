ea553809f6552e37329c45797900b7546d5ff012
testname <testemail>
2018-09-06 15:14:24 +1000

    more stuff exercises done

---
+++ more_stuff_exercises.rb/exercises.rb
@@ -0,0 +1,30 @@
+# 1.
+# def check
+#   words = ['laboratory','experiment','Pans Labyrinth','elaborate','polar bear']
+#   words.each do |word|
+#     if word =~ /lab/
+#       puts word
+#     end
+#   end
+# end
+
+# check()
+
+
+# 2. missing "call". returns nothing
+
+# 3.
+# exception handling is for when your code runs into an error, it will know what to do thanks to exception handling. otherwise,
+# your code will crash
+
+
+# 4.
+# def execute(&block)
+#   block.call
+# end
+
+# execute { puts "Hello from inside the execute method!" }
+
+# 5.
+# there is no & symbol which represents an argument for a  block
+
