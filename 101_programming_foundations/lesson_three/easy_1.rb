eea35d935200a887a5a6cba7394a50d26399d088
testname <testemail>
2018-09-19 11:37:36 +1000

    easy-1 done

---
+++ 101_programming_foundations/lesson_three/easy_1.rb
@@ -0,0 +1,45 @@
+# 1.
+# the original numbers array because the uniq method does not mutate the caller
+
+# 2.
+# != is not equal
+# ! can be for destructive method.
+# ? can be for ternary operator
+# !! turns it into a boolean
+
+# 3.
+# advice = "Few things in life are as important as house training your pet dinosaur."
+# advice.gsub!('important', 'urgent')
+
+# 4.
+# numbers = [1, 2, 3, 4, 5]
+
+# numbers.delete_at(1)
+# numbers.delete(1)
+# delete_at refers to the index. delete(1) means removing number 1
+
+# 5.
+# 42.between?(0, 100)
+
+# 6.
+# famous_words = "seven years ago..."
+# puts "Four score and " + famous_words
+# puts "Four score and #{famous_words}"
+
+# 7.
+# 42
+
+# 8.
+# flintstones = ["Fred", "Wilma"]
+# flintstones << ["Barney", "Betty"]
+# flintstones << ["BamBam", "Pebbles"]
+
+# p flintstones.flatten
+
+# 9.
+# flintstones = {
+#   "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5
+# }
+
+# flintstones.assoc('Barney')
+
