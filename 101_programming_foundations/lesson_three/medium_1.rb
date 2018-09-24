b827607256743da1a6f2f3d769a5c07a3238c7b6
testname <testemail>
2018-09-24 10:16:11 +1000

    medium 1 and 2 done

---
+++ 101_programming_foundations/lesson_three/medium_1.rb
@@ -0,0 +1,38 @@
+# 1.
+# 10.times do |n|
+#   puts "#{(" " * n  )} The Flinstones Rock!"
+# end
+
+# 2.
+# Error because you are adding integer into string.
+# You can use interpolation or .to_s
+
+# 3.
+# while dividend > 0
+#   divisors << number / dividend if number % dividend == 0
+#   dividend -= 1
+# end
+
+# bonus 1: checks if the number is odd or not
+# bonus 2: it is to return the divisors.
+
+# 4.
+# yes. the << will mutate the caller, the + will not
+
+# 5.
+# the scope of limit is outside the fib method
+
+# 6.
+# I do not understand the question
+
+# 7.
+# 34
+
+# 8.
+# yes, the original munsters is affected
+
+# 9.
+# paper
+
+# 10.
+# 'no'
---
+++ 101_programming_foundations/lesson_three/medium_2.rb
@@ -0,0 +1,20 @@
+# 3.
+# a = 'hello'
+# b = ['jon','hugo']
+
+# def tricky_method(a_string_param, an_array_param)
+#   a_string_param += "rutabaga"
+#   an_array_param << "rutabaga"
+# end
+
+# p tricky_method(a, b)
+# p b
+
+# 4.
+# pumpkinsrutabaga
+# [pumpikns]
+
+# 5.
+# def color_valid(color)
+#   color == "blue" || color == "green"
+# end
